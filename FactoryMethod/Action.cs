2f1bcfe7a25d91be86cbd7e0f4074b2f3b92c641
thx4u <xavavka12345@gmail.com>
2020-10-20 20:12:45 +0300

    fix

--- FactoryMethod/Action.cs
+++ FactoryMethod/Action.cs
@@ -5,17 +5,17 @@ namespace FactoryMethod
     abstract class Action
     {
         private string actionDate;
-        private string actionType;
+        private string actionLink;
 
-        public Action(string date, string action)
+        public Action(string date, string actionLink)
         {
             actionDate = date;
-            actionType = action;
+            this.actionLink = actionLink;
         }
 
         public IProduct Print()
         {
-            Console.WriteLine("\nDate: {0}\nAction: {1}",actionDate,actionType);
+            Console.WriteLine("\nDate: {0}\nLink: {1}",actionDate,actionLink);
             return null;
         }
         public abstract IProduct GetDescription();
@@ -27,7 +27,7 @@ namespace FactoryMethod
         {
             return new UkraineDescriptions();
         }
-        public ConcreteCreator1(string date, string action) : base(date, action)
+        public ConcreteCreator1(string date, string actionLink) : base(date, actionLink)
         {
         }
     }
@@ -38,7 +38,7 @@ namespace FactoryMethod
             return new EnglishDescriptions();
         }
 
-        public ConcreteCreator2(string date, string action) : base(date, action)
+        public ConcreteCreator2(string date, string actionLink) : base(date, actionLink)
         {
         }
     }
--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -7,18 +7,18 @@ namespace FactoryMethod
         static void Main(string[] args)
         {
             Action action;
-            string actionType = "Death";
+            string actionLink = "https://en.wikipedia.org/wiki/Assassination_of_John_F._Kennedy";
             string date = "22.11.1963";
             Console.WriteLine("Enter what leng u want");
             Console.WriteLine("1. Ukraine description\n2. English description");
             int n = int.Parse(Console.ReadLine());
             if (n == 1)
             {
-                action = new ConcreteCreator1(date,actionType);
+                action = new ConcreteCreator1(date,actionLink);
             }
             else
             {
-                action = new ConcreteCreator2(date,actionType);
+                action = new ConcreteCreator2(date,actionLink);
             }
             
             IProduct productPrint = action.Print();
